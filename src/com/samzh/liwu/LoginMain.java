b4e3dbb2e08b5d8888341a3486d961a41981d33b
samzh.zz <samzh.zz@f757b9e6-42eb-04a6-0ed9-f1cd1cab7a45>
2011-06-28 09:50:04 +0000

    project initialized.

---
+++ src/com/samzh/liwu/LoginMain.java
@@ -0,0 +1,13 @@
+package com.samzh.liwu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginMain extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
